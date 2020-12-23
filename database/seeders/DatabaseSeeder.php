0a92469567093faaf816d75e7437a82291405a3e
osman forhad <osmanforhad@pop-os.localdomain>
2020-12-23 22:16:51 +0600

    minor correction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-    }
-}
+        \App\Models\Admin::factory()->create();
+    }//end of the run method
+    
+}//end of the DatabaseSeeder class
