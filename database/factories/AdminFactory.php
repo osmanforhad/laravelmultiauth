89ac73fb585a4e7437b75baf15ed07383a2ccfb2
osman forhad <osmanforhad@pop-os.localdomain>
2020-12-23 22:12:26 +0600

    working with Admin factory and admin migration

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -22,7 +22,12 @@ class AdminFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => 'Osman Forhad',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
-    }
-}
+    }//end of the definition method
+
+}//end of the AdminFactory class
